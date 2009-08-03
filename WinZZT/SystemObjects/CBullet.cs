0d775e7c08c5120e63231f969e8835b5ec1f4b28
robholx <robholx@571fbde6-8034-11de-8c42-8d293cda74e2>
2009-08-03 13:50:52 +0000

    Yawn.

---
+++ WinZZT/SystemObjects/CBullet.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WinZZT
+{
+    class CBullet : CTimedElement
+    {
+        public EDirection Direction;
+        public CElement Source;
+
+        public CBullet(int x, int y, int cCycle, EDirection direction, CElement source)
+        {
+            this.BackColor = Color.FromArgb(0,0,0,0);
+            this.ForeColor = Color.White;
+            this.Char = "∙";
+            this.Block = true;
+
+            this.Source = source;
+
+            this.Ordering = 999;
+
+            Direction = direction;
+            this.Cycle = cCycle;
+            this.Location = new Point(x, y);
+            base.InitPosition(x, y);
+            base.Init(cCycle);
+        }
+
+        public override void Step()
+        {
+
+            Point p = CGrid.GetInDirection(Location, Direction);
+
+            if (!Try(Direction,false))
+            {
+                
+
+                if (!CGrid.IsValid(p))
+                {
+                    Die();
+                    return;
+                }
+
+
+                    CElement target = CGrid.Get(p).GetTopmost();
+                    if (target != null && target.BlockBullets)
+                    {
+                        target.Shot(Source);
+                        Die();
+                    }
+                    else
+                        Move(Location, p);
+
+            }
+            else
+                Move(Location, p);
+
+
+        }
+
+    }
+}

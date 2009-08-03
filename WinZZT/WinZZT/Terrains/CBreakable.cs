59cfeb45a2410dd434322eb1ea71d9737d2b549b
robholx <robholx@571fbde6-8034-11de-8c42-8d293cda74e2>
2009-08-03 13:55:16 +0000

    Water anyone?

---
+++ WinZZT/WinZZT/Terrains/CBreakable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+
+namespace WinZZT
+{
+    class CBreakable : CElement
+    {
+        public CBreakable(int x, int y, Color c)
+        {
+            InitProps(x, y, "▒", c, Color.Black, true, 0);
+        }
+
+        public override void Shot(CElement responsible)
+        {
+            this.Die();
+        }
+
+    }
+}
---
+++ WinZZT/WinZZT/frmMain.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace WinZZT
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pb = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb
+            // 
+            this.pb.BackColor = System.Drawing.Color.Black;
+            this.pb.Location = new System.Drawing.Point(12, 12);
+            this.pb.Name = "pb";
+            this.pb.Size = new System.Drawing.Size(746, 433);
+            this.pb.TabIndex = 1;
+            this.pb.TabStop = false;
+            this.pb.Click += new System.EventHandler(this.pb_Click);
+            this.pb.Paint += new System.Windows.Forms.PaintEventHandler(this.pb_Paint);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(770, 464);
+            this.Controls.Add(this.pb);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "frmMain";
+            this.Text = "WinZZT";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pb)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.PictureBox pb;
+
+    }
+}
+

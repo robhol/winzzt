db08a0e3a76d21767944e55fbf7b6b4bd2d7ab28
robholx <robholx@571fbde6-8034-11de-8c42-8d293cda74e2>
2009-08-07 21:44:22 +0000

    Added a console for debug commands, cheats, whatever

---
+++ WinZZT/frmConsole.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WinZZT
+{
+    public partial class frmConsole : Form
+    {
+        public frmConsole()
+        {
+            InitializeComponent();
+        }
+
+        private void OutputConsole(string s)
+        {
+            txtConsole.Text += s + "\r\n";
+        }
+
+        private void HandleCommand(string raw)
+        {
+
+            string[] args = raw.Split(" ".ToCharArray());
+
+            switch (args[0])
+            {
+                case "heal":
+                    {
+
+                        if (args.Length == 1)
+                        {
+                            CGame.PlayerHealth = 100;
+                        }
+                        else
+                        {
+                            CGame.PlayerHealth += int.Parse(args[1]);
+                        }
+                        
+
+                        OutputConsole("Player healed.");
+                        break;
+                    }
+                case "exit":
+                    {
+                        this.Close();
+                        break;
+                    }
+                default:
+                    {
+                        OutputConsole("Unknown command.");
+                        break;
+                    }
+            }
+
+
+        }
+
+        private void txtConsole_Enter(object sender, EventArgs e)
+        {
+            txtInput.Focus();
+        }
+
+        private void txtInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                HandleCommand(txtInput.Text);
+                txtInput.Text = "";
+            }
+            else if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void frmConsole_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

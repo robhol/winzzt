c849777b5153c92fb17e4a61e7552bca568fe6b1
robholx <robholx@571fbde6-8034-11de-8c42-8d293cda74e2>
2009-08-13 18:25:24 +0000

    Other optimization for drawing. This method should help a lot more than the previous one. Big thanks to |d13m0b.

--- trunk/WinZZT/CDrawing.cs
+++ trunk/WinZZT/CDrawing.cs
@@ -1,7 +1,7 @@
 ﻿/*
-
+ 
 CDrawing.cs
-
+ 
 Contains members and methods relating to drawing.
   
 */
@@ -13,28 +13,75 @@ using System.Text;
 using System.Drawing;
 using System.Timers;
 
+using System.Collections;
+
 namespace WinZZT
 {
+    /* Wrapper for the image */
+    class BitmapCharacter
+    {
+        public BitmapCharacter(Color color, int character)
+        {
+            this.Bitmap = CCharManager.GetChar(character, color) as Bitmap;
+            this.Color = color;
+            this.Char = character;
+        }
+
+        public Bitmap Bitmap { get; set; }
+        public Color Color { get; set; }
+        public int Char { get; set; }
+
+        public int SearchValue /* Unique search value for Dictionary */
+        {
+            get
+            {
+                return CalculateSearch(this.Color, this.Char);
+            }
+        }
+
+        public static int CalculateSearch(Color color, int character)
+        {
+            return (color.R + color.G + color.B) + character;
+        }
+    }
+
     static class CDrawing
     {
-
         public static Size CellSize = new Size(8, 14);
 
         public static Size CanvasSize = new Size(320, 350);
 
         //Font for text at the bottom of the screen.
-        public static Font DrawFont = new Font("Trebuchet MS", 12); 
+        public static Font DrawFont = new Font("Trebuchet MS", 12);
 
         //Font used in the side bar/status bar
-        public static Font BarFont = new Font("Courier New", 8); 
+        public static Font BarFont = new Font("Courier New", 8);
 
         public static StringFormat stringFormat = new StringFormat();
+
+        private static Dictionary<int, BitmapCharacter> DrawableCharacters = new Dictionary<int, BitmapCharacter>();
+
+        [Obsolete]
+        private static Dictionary<int, Bitmap> LoadBitmapCharacters()
+        {
+            Dictionary<int, Bitmap> characters = new Dictionary<int, Bitmap>();
+            Bitmap currBitmap;
+
+            for (int i = 1; i < 256; i++)
+            {
+                currBitmap = CCharManager.GetChar(i, Color.Wheat) as Bitmap;
+                characters.Add(i, currBitmap);
+            }
+
+            return characters;
+        }
+
 
         //Whether or not to draw the grid.
         public static bool DrawDGrid = false;
 
         public static Timer tmrDraw;
-            
+
         public static void Initialize()
         {
             stringFormat.Alignment = StringAlignment.Center;
@@ -44,10 +91,9 @@ namespace WinZZT
             tmrDraw.Interval = 30;
             tmrDraw.Elapsed += new ElapsedEventHandler(tmrDraw_Elapsed);
             tmrDraw.Start();
-
         }
 
-        static DateTime textExpiration = new DateTime(1999,1,1);
+        static DateTime textExpiration = new DateTime(1999, 1, 1);
         static string textValue = "";
         static bool textDisplayActive = false;
 
@@ -102,13 +148,21 @@ namespace WinZZT
 
             g.FillRectangle(new SolidBrush(o.BackColor), p.X, p.Y, CellSize.Width, CellSize.Height);
 
-            g.DrawImage(CCharManager.GetChar(o.Char, o.ForeColor), p);
+            int toSearch = BitmapCharacter.CalculateSearch(o.BackColor, o.Char);
 
+            BitmapCharacter loadedBitmap;
+            if (!DrawableCharacters.ContainsKey(toSearch))
+            {
+                loadedBitmap = new BitmapCharacter(o.ForeColor, o.Char);
+                DrawableCharacters.Add(toSearch, loadedBitmap);
+            }
+
+            g.DrawImage(DrawableCharacters[toSearch].Bitmap, p);
         }
 
         public static void DrawTile(int x, int y, Graphics g)
         {   //Draws a certain tile
-            Point gp = new Point(x,y);
+            Point gp = new Point(x, y);
             Point p = GetCanvasCoords(gp);
 
             if (DrawDGrid)
@@ -131,7 +185,7 @@ namespace WinZZT
 
             }
 
-            
+
         }
 
         public static void DrawBar(Graphics g)
@@ -140,13 +194,13 @@ namespace WinZZT
             StringFormat sfCenter = new StringFormat();
             sfCenter.LineAlignment = StringAlignment.Center;
 
-            g.FillRectangle(new SolidBrush(Color.FromArgb(0,0,100)), CanvasSize.Width, 0, 100, CanvasSize.Height);
+            g.FillRectangle(new SolidBrush(Color.FromArgb(0, 0, 100)), CanvasSize.Width, 0, 100, CanvasSize.Height);
 
-            g.DrawString("- WinZZT -", BarFont, Brushes.Yellow,CanvasSize.Width + 18, 30, sfCenter);
+            g.DrawString("- WinZZT -", BarFont, Brushes.Yellow, CanvasSize.Width + 18, 30, sfCenter);
 
             string status =
-                "Health :  " + CGame.PlayerHealth.ToString().PadLeft(3,char.Parse(" ")) + "\r\n" +
-                "Ammo   : "  + CGame.PlayerAmmo.ToString().PadLeft(4, char.Parse(" "))
+                "Health :  " + CGame.PlayerHealth.ToString().PadLeft(3, char.Parse(" ")) + "\r\n" +
+                "Ammo   : " + CGame.PlayerAmmo.ToString().PadLeft(4, char.Parse(" "))
                 ;
 
             g.DrawString(status, BarFont, Brushes.White, CanvasSize.Width + 5, 50);
@@ -157,8 +211,8 @@ namespace WinZZT
             int d = 0;
             foreach (KeyValuePair<Color, int> k in CGame.KeyList)
             {
-                Point p = CUtil.addPoints(kdOffset, new Point(40 * (int)Math.Floor((double)d / 4),13 * (d % 4)));
-                DrawSymbol(12,p,k.Key,Color.Transparent,g);
+                Point p = CUtil.addPoints(kdOffset, new Point(40 * (int)Math.Floor((double)d / 4), 13 * (d % 4)));
+                DrawSymbol(12, p, k.Key, Color.Transparent, g);
                 if (k.Value > 1)
                     g.DrawString(k.Value.ToString(), BarFont, Brushes.White, p.X + 6, p.Y - 1);
                 d++;
@@ -182,7 +236,7 @@ namespace WinZZT
                 f.Alignment = StringAlignment.Center;
 
                 g.FillRectangle(new SolidBrush(Color.FromArgb(50, 255, 0, 0)), 0, 0, CanvasSize.Width, CanvasSize.Height);
-                g.DrawString("GAME OVER", new Font("Courier New", 14, FontStyle.Bold), Brushes.White, CanvasSize.Width / 2, CanvasSize.Height / 2,f);
+                g.DrawString("GAME OVER", new Font("Courier New", 14, FontStyle.Bold), Brushes.White, CanvasSize.Width / 2, CanvasSize.Height / 2, f);
             }
 
             if (textDisplayActive)
@@ -191,7 +245,7 @@ namespace WinZZT
                 StringFormat f = new StringFormat();
                 f.Alignment = StringAlignment.Center;
 
-                g.DrawString(textValue, DrawFont, Brushes.White, new Rectangle(0, CanvasSize.Height - CellSize.Height - 7, CanvasSize.Width, CellSize.Height + 7),f);
+                g.DrawString(textValue, DrawFont, Brushes.White, new Rectangle(0, CanvasSize.Height - CellSize.Height - 7, CanvasSize.Width, CellSize.Height + 7), f);
 
             }
 
@@ -199,11 +253,11 @@ namespace WinZZT
 
         }
 
-        public static void ProvokeDrawing( bool force )
+        public static void ProvokeDrawing(bool force)
         {   //Forces drawing
 
             if (force && Program.MainForm != null)
-            Program.MainForm.pb.Invalidate();
+                Program.MainForm.pb.Invalidate();
         }
 
     }
